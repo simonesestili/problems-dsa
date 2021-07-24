b42d83d25eb3be014aee26b799e97d812fc6d510
Simone <yolodoggy911@gmail.com>
2021-07-24 00:52:05 -0700

    save

--- 1926.py
+++
@@ -1,2 +0,0 @@
-class Solution:
-    def nearestExit(self, maze: List[List[str]], entrance: List[int], row=-1, col=-1, steps=0) -> int:
--- pyramidSlideDown.py
+++ pyramidSlideDown.py
@@ -1,10 +1,12 @@
+# Codewars problem
+
 def longest_slide_down(pyramid, level=-1):
-    if level == -1: level = len(pyramid) - 1
-    if level == 0: return pyramid[0][0]
+    if level == -1:
+        level = len(pyramid) - 1
+    if level == 0:
+        return pyramid[0][0]
 
     for i in range(len(pyramid[level]) - 1):
         pyramid[level-1][i] += max(pyramid[level][i], pyramid[level][i + 1])
 
     return longest_slide_down(pyramid, level-1)
-
-print(longest_slide_down([[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]]))
