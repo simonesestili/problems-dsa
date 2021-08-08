0ba9d9c17c945ff184e681f719f42f8d64ca9eeb
Simone <yolodoggy911@gmail.com>
2021-08-07 22:54:21 -0700

    1814

---
+++ 1814.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countNicePairs(self, nums: List[int]) -> int:
+        total = 0
+        seen = {}
+        for num in nums:
+            diff = num - self.rev(num)
+            if diff in seen:
+                total += seen[diff]
+            seen[diff] = seen.get(diff, 0) + 1
+        return total % (1e9 + 7)
+
+    def rev(self, n):
+        return int(str(n)[-1::-1])
