a2eb04eb2ac672f17c5bc1c7f1adc9645c4b6372
Simone <yolodoggy911@gmail.com>
2021-08-19 21:12:50 -0700

    taskAssignment

--- 2.py
+++ 2.py
@@ -2,7 +2,7 @@
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
-#         self.next = next 
+#         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         res = ListNode()
