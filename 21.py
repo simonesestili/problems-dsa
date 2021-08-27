249eb593794c15b43c1ee98b0dbb2ab38a016b69
unknown <yolodoggy911@gmail.com>
2021-08-27 13:48:31 -0700

    s

--- 21.py
+++ 21.py
@@ -3,7 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
+class Solution: 
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         if not l1 or not l2:
             if not l1:
