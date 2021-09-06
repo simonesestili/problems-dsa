914f4f3e4a2b5da20b597647074fe8b14660eeb4
Simone <yolodoggy911@gmail.com>
2021-09-05 21:04:04 -0700

    readme

---
+++ 268.java
@@ -0,0 +1,11 @@
+class Solution {
+	public int missingNumber(int[] nums) {
+		int n = nums.length;
+		int total = n * (n + 1) / 2;
+		int sum = 0;
+		for(int num : nums)
+			sum += num;
+		return total - sum;
+	} 
+
+}
