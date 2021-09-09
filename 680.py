f34177fbc99f9a1f75595cc61ef7b3223a1691cb
unknown <yolodoggy911@gmail.com>
2021-09-09 13:38:22 -0700

    2 prblems

--- 680.py
+++ 680.py
@@ -1,21 +1,5 @@
 class Solution(object):     
     def validPalindrome(self, s): 
         left, right = 0, len(s) - 1
-        removed = False
-        for i in range(len(s) // 2):
-            if s[left] != s[right]:
-                if removed:
-                    return False
 
-                if s[left + 1] == s[right]:
-                    left += 1
-                elif s[left] == s[right - 1]:
-                    right -= 1
-                else:
-                    return False
-
-                removed = True
-            left += 1
-            right -= 1
-
-        return True
+        while left < right
