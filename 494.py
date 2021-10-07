59f8869e8807799fe661395c642cf16f9138e409
unknown <yolodoggy911@gmail.com>
2021-10-07 09:44:24 -0700

    494

--- 494.py
+++ 494.py
@@ -1,10 +1,12 @@
 class Solution:
-	# TLE
-	def findTargetSumWays(self, nums: List[int], target: int) -> int:
-		return self.sumways(nums, target, 0, 0)
+    def findTargetSumWays(self, nums, target):
+        ways = defaultdict(int)
+        ways[0] = 1
+        for num in nums:
+            next_ways = defaultdict(int)
+            for curr in ways:
+                next_ways[curr - num] += ways[curr]
+                next_ways[curr + num] += ways[curr]
+            ways = next_ways    
+        return ways[target]    
 
-	def sumways(self, nums, target, curr, idx):
-		if idx == len(nums):
-			return 1 if target == curr else 0
-		return (self.sumways(nums, target, curr + nums[idx], idx + 1) +
-			self.sumways(nums, target, curr - nums[idx], idx + 1)) 	
