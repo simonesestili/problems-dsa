509101353a2fb1bfa1b8534fae3420ff1bf5f3eb
Simone <yolodoggy911@gmail.com>
2021-10-20 22:06:21 -0700

    380

--- 380.py
+++ 380.py
@@ -1,24 +1,48 @@
 class RandomizedSet:
     def __init__(self):
-        self.map = {}
         self.nums = []
+        self.indices = {} # val: idx
 
-    def insert(self, val: int) -> bool:
-        if val not in self.map:
-            self.map[val] = len(self.nums)
-            self.nums.append(val)
-            return True
-        return False
+    def insert(self, val):
+        if val in self.indices:
+            return False
+        self.indices[val] = len(self.nums)
+        self.nums.append(val)
+        return True
 
-    def remove(self, val: int) -> bool:
-        if val in self.map:
-            del self.map[val]
-            return True
-        return False
+    def remove(self, val):
+        if val not in self.indices:
+            return False
+        last, idx = self.nums[-1], self.indices[val]
+        self.nums[idx], self.indices[last] = last, idx
+        del self.indices[val]
+        self.nums.pop()
+        return True
 
-    def getRandom(self) -> int:
-        n = random.randint(0, len(self.nums) - 1)
-        while self.nums[n] not in self.map:
-            n = random.randint(0, len(self.nums) - 1)
-        return self.nums[n]
+    def getRandom(self):
+        return self.nums[random.randint(0, len(self.nums) - 1)]
 
+# from random import randint
+# class RandomizedSet:
+#     def __init__(self):
+#         self.set = set()
+#         self.nums = list()
+# 
+#     def insert(self, val):
+#         if val in self.set:
+#             return False
+#         self.set.add(val)
+#         self.nums.append(val)
+#         return True
+# 
+#     def remove(self, val):
+#         if val not in self.set:
+#             return False
+#         self.set.remove(val)
+#         return True
+# 
+#     def getRandom(self):
+#         n = randint(0, len(self.nums) - 1)
+#         while self.nums[n] not in self.set:
+#             n = randint(0, len(self.nums) - 1)
+#         return self.nums[n]    
