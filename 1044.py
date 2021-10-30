9081fce343c60fffec0f196c232437d066277e44
Simone <yolodoggy911@gmail.com>
2021-10-30 12:28:34 -0700

    1044

--- 1044.py
+++ 1044.py
@@ -1,38 +1,39 @@
 class Solution:
+    # The length of the longest duplicate substring must be in the range [0, n- 1]
+    # With this fact binary search can be used to limit the amount of total checks required
+    # Each check will consist of using a rolling hash in order to check for uniqueness
+    # This rolling hash is essentially a sliding window of the substring in s
+    # This means that computing the next hash to the right can be done in O(1)
     def longestDupSubstring(self, s):
-        self.s = s
-        ans, n = '', len(s)
-        lo, hi = 0, n - 1
-        while lo < hi:
-            cand = (lo + hi + 1) // 2
-            hash_val = self.hash(cand)
-            seen = {}
-            found = False
-            for i in range(len(s) - cand):
-                if hash_val in seen and s[i:i + cand] == s[seen[hash_val][0]:seen[hash_val][1]]:
-                    ans = s[i:i + cand] 
-                    found = True
-                    break
-                seen[hash_val] = (i, i + cand)
-                hash_val = self.next_hash(hash_val, i, i + cand)
-            if hash_val in seen and s[i:i + cand] == s[seen[hash_val][0]:seen[hash_val][1]]:
-                ans = s[i + 1:i + 1 + cand] 
-                found = True
-            if found:
-                lo = cand
+        vals = [ord(c) - ord('a') for c in s]
+        mod = 2 ** 31 - 1
+
+        def getDup(length):
+            val = 0
+            for i in range(length):
+                val = (val * 26 + vals[i]) % mod
+            print(val)
+            seen = defaultdict(list)    
+            seen[val].append((0, length))
+            # Calculate rolling hashes
+            for i in range(len(s) - length):
+                factor = pow(26, length, mod)
+                val = (val * 26 + vals[i + length] - vals[i] * factor) % mod
+                if val in seen:
+                    # Validate correctness since hash collisions giving false positivies is possible
+                    for start, end in seen[val]:
+                        if s[start:end] == s[i + 1:i + 1 + length]:
+                            return (start, end)
+                seen[val].append((i + 1, i + 1 + length))    
+
+        left, right = 0, len(s) - 1
+        ans = (0, 0)
+        while left < right:
+            cand = (left + right + 1) // 2
+            dup = getDup(cand)
+            if dup:
+                ans = dup
+                left = cand
             else:
-                hi = cand - 1 
-
-        return ans
-
-    def next_hash(self, curr, left, right):
-        mod = 10 ** 9 + 7
-        length = right - left
-        return ((ord(self.s[right]) - ord('a')) * 26 ** (length - 1) + curr // 26) % mod
-
-    def hash(self, i):
-        mod = 10 ** 9 + 7 
-        val = 0
-        for j in range(i):
-            val = (val + (ord(self.s[j]) - ord('a')) * 26 ** j) % mod
-        return val    
+                right = cand - 1
+        return s[ans[0]:ans[1]]
