3d0dea203f4a3f6dac93c37996ee1f886f9fc3fd
Simone Sestili <75584972+simonesestili@users.noreply.github.com>
2021-11-06 17:05:32 -0700

    Delete taskAssignment.py

--- misc/taskAssignment.py
+++
@@ -1,7 +0,0 @@
-def taskAssignment(k, tasks):
-	tasks = [(i, tasks[i]) for i in range(len(tasks))]
-	tasks = sorted(tasks, key=lambda task: task[1])
-	optimal = []
-	for i in range(len(tasks) // 2):
-		optimal.append([tasks[i][0], tasks[len(tasks) - 1 - i][0]])
-	return optimal
