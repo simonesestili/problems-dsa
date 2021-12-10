9aae931d1ee1c1d06f90e8457f0a150a0e3594b8
Simone <yolodoggy911@gmail.com>
2021-12-09 18:09:48 -0800

    1306 790

--- 1306.py
+++ 1306.py
@@ -1,6 +1,6 @@
 class Solution:     
-    def canReach(self, arr: List[int], start: int) -> bool: 
+    def canReach(self, arr, start):
         if 0 <= start < len(arr) and arr[start] >= 0:
-            arr[start] = -arr[start]
-            return arr[start] == 0 or self.canReach(arr, start - arr[start]) or self.canReach(arr, start + arr[start]) 
-        return False    
+            arr[start] *= -1
+            return not arr[start] or self.canReach(arr, start - arr[start]) or self.canReach(arr, start + arr[start])
+        return False
--- 790.py
+++ 790.py
@@ -1,8 +1,13 @@
 class Solution:
     def numTilings(self, n):
-        MOD = 10 ** 9 + 7
-        full, half = [1, 1], [0, 0]
+        M = 10 ** 9 + 7
+        prev_full, full = 1, 1
+        prev_half, half = 0, 0
+        
         for _ in range(n - 1):
-            new_full, new_half = full[0] + full[1] + half[1], half[1] + 2 * full[0]
-            full, half = [full[1], new_full % MOD], [half[1], new_half % MOD]
-        return full[1]
+            new_full = prev_full + full + half
+            new_half = half + prev_full * 2
+            prev_full, full = full, new_full % M
+            prev_half, half = half, new_half % M
+
+        return full
