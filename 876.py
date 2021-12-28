1a49aa06ab0218bd2055dc3d4f2dff9bf011e511
Simone <yolodoggy911@gmail.com>
2021-12-27 16:01:47 -0800

    876

--- 876.py
+++ 876.py
@@ -1,10 +1,6 @@
 class Solution:
 	def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-		slow, fast = head, head
-		while fast and fast.next:
-			slow = slow.next
-			fast = fast.next.next	
-
-		return slow
-	
-
+        slow = fast = head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+        return slow
