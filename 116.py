0fe9e13239c6b8ac3c7235d2f008625466ce74e0
Simone <yolodoggy911@gmail.com>
2021-12-28 16:20:10 -0800

    116

--- 116.py
+++ 116.py
@@ -1,28 +1,12 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
-        self.val = val
-        self.left = left
-        self.right = right
-        self.next = next
-"""
-
 class Solution:
-    def connect(self, root: 'Node') -> 'Node':
-        if not root:
-            return root
-        curr = [root]
-        while curr:
-            next_layer = []
-            # Settings nexts
-            for i in range(len(curr)):
-                if curr[i].left:
-                    next_layer.append(curr[i].left)
-                    next_layer.append(curr[i].right)
-                if i == len(curr) - 1:
-                    curr[i].next = None
-                else:
-                    curr[i].next = curr[i + 1]
-            curr = next_layer
+    def connect(self, root):
+        level = [root] if root else []
+        while level:
+            next_level = []
+            for i, node in enumerate(level):
+                if i: level[i - 1].next = node
+                if not node.left: continue
+                next_level.append(node.left)
+                next_level.append(node.right)
+            level = next_level
         return root
