0c876d2b4a68cced7cd664e513c1005b6d7eaea3
Simone <yolodoggy911@gmail.com>
2022-01-16 12:41:01 -0800

    654

--- 654.py
+++ 654.py
@@ -1,14 +1,11 @@
 class Solution:     
 	def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
-		return self.construct(nums, 0, len(nums) - 1)
-	
-	def construct(self, nums, left, right):
-		if left > right:
-			return None
-		max_idx = left
-		for i in range(left, right + 1):
-			if nums[i] > nums[max_idx]:
-				max_idx = i
-		return TreeNode(nums[max_idx], self.construct(nums, left, max_idx - 1), self.construct(nums, max_idx + 1, right))
-		
-			
+
+        def build(left, right):
+            mx = left
+            if right < left: return None
+            for i in range(left, right + 1):
+                if nums[i] > nums[mx]: mx = i
+            return TreeNode(nums[mx], left=build(left, mx - 1), right=build(mx + 1, right))
+        
+        return build(0, len(nums) - 1)
