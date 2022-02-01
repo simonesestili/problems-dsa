212f806136ad5894d4ed6645732ddea6a48e432a
Simone <yolodoggy911@gmail.com>
2022-01-31 16:18:38 -0800

    121 1672

--- 121.py
+++ 121.py
@@ -1,13 +1,9 @@
 class Solution:
-	def maxProfit(self, prices: List[int]) -> int:
-		max_right = [-1] * len(prices)
-		curr_max = -1
-		for i in range(len(prices) - 1, -1, -1):
-			max_right[i] = curr_max
-			curr_max = max(curr_max, prices[i])
-		
-		max_profit = 0
-		for i in range(len(prices)):
-			max_profit = max(max_profit, max_right[i] - prices[i])
-		
-		return max_profit
+	def maxProfit(self, prices):
+        profit, buy = 0, float('inf')
+
+        for price in prices:
+            profit = max(profit, price - buy)
+            buy = min(buy, price)
+
+        return profit
