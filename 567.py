ff4e403ab754d93db470a4facebafb4f34820a40
Simone <yolodoggy911@gmail.com>
2022-02-10 20:24:30 -0800

    567

--- 567.py
+++ 567.py
@@ -1,24 +1,45 @@
 class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
+    def checkInclusion(self, s1, s2):
+        m, n = len(s1), len(s2)
+        if m > n: return False
+        code = lambda c: ord(c) - ord('a')
+        decrement = lambda x: -1 if x == 0 else 1 if x == -1 else 0
+        increment = lambda x: -1 if x == 0 else 1 if x == 1 else 0
 
-        target = [0] * 26
-        for letter in s1:
-            target[ord(letter) - ord('a')] += 1
+        delta, diff = [0] * 26, 0
+        for c in s1:
+            diff += delta[code(c)] == 0
+            delta[code(c)] += 1
+
+        for i in range(m - 1):
+            idx = code(s2[i])
+            delta[idx] -= 1
+            diff += decrement(delta[idx])
+
+        for left in range(n - m + 1):
+            l, r = code(s2[left]), code(s2[left + m - 1])
+            delta[r] -= 1
+            diff += decrement(delta[r])
+            if diff == 0: return True
+            delta[l] += 1
+            diff += increment(delta[l])
+
+        return False
+
+class Solution2:
+    def checkInclusion(self, s1, s2):
+        m, n = len(s1), len(s2)
+        if m > n: return False
+        code = lambda c: ord(c) - ord('a')
         
-        window = [0] * 26
-        for i in range(len(s1)):
-            window[ord(s2[i]) - ord('a')] += 1
-        if target == window:
-            return True
+        delta = [0] * 26
+        for c in s1: delta[code(c)] += 1
+        for i in range(m - 1): delta[code(s2[i])] -= 1
 
-        left, right = 0, len(s1)
-        while right < len(s2):
-            window[ord(s2[left]) - ord('a')] -= 1
-            left += 1
-            window[ord(s2[right]) - ord('a')] += 1
-            right += 1
-            if target == window:
-                return True
+        for left in range(n - m + 1):
+            l, r = code(s2[left]), code(s2[left + m - 1])
+            delta[r] -= 1
+            if not any(delta): return True
+            delta[l] += 1
+
         return False
