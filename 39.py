58bf8ee37378e2453e54d9fc1611698ed5d7c771
Simone <yolodoggy911@gmail.com>
2022-02-16 17:32:17 -0800

    39

--- 39.py
+++ 39.py
@@ -2,14 +2,14 @@ class Solution:
     def combinationSum(self, cands, target):
         self.ans = []
 
-        def dfs(curr, i, target):
-            if target <= 0:
-                if not target: self.ans.append(curr[:])
+        def dfs(curr, i, delta):
+            if delta <= 0:
+                if not delta: self.ans.append(curr[:])
                 return
             if i >= len(cands):
                 return
-            dfs(curr + [cands[i]], i, target - cands[i])
-            dfs(curr[:], i + 1, target)
+            dfs(curr + [cands[i]], i, delta - cands[i])
+            dfs(curr, i + 1, delta)
 
         dfs([], 0, target)
         return self.ans
