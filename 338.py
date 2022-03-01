776d6b68fcc66332035d9325efff23445c6e8af9
unknown <yolodoggy911@gmail.com>
2022-03-01 13:41:35 -0800

    338

--- 338.py
+++ 338.py
@@ -1,11 +1,6 @@
 class Solution:     
     def countBits(self, n):
         ans = [0] * (n + 1)
-        for i in range(n + 1):
-            j = i
-            while j:
-                ans[i] += j & 1
-                j >>= 1
+        for i in range(1, n + 1):
+            ans[i] = ans[i >> 1] + (i & 1)
         return ans
-
-
