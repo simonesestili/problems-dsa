1aba2126b9e3a74fe1b72475fa0b79cbcb390282
Simone <yolodoggy911@gmail.com>
2022-03-05 16:29:15 -0800

    1359

--- 1359.py
+++ 1359.py
@@ -2,6 +2,7 @@ class Solution:
     def countOrders(self, n):
         count, M = 1, 10 ** 9 + 7
         for i in range(1, n + 1):
+            # m represents the number of possible slots we can place the new Pi and Di
             m = i << 1
-            count = (count * (m * (m - 1) // 2 % M)) % M
+            count = count * (m * (m - 1) >> 1 % M) % M
         return count
