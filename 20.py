664607d64cb283840c829ab446e2546ee9ca2735
unknown <yolodoggy911@gmail.com>
2022-03-12 19:02:17 -0800

    20

--- 20.py
+++ 20.py
@@ -1,13 +1,10 @@
 class Solution:
-    def isValid(self, s: str) -> bool:
-        stack = []
-        for let in s:
-            if let in '([{':
-                stack.append(let)
-            else:
-                if not stack:
-                    return False
-                removed = stack.pop()
-                if removed + let not in '(){}[]':
-                    return False
-        return len(stack) == 0
+    def isValid(self, s):
+        stack, matches = [], {')':'(', '}':'{', ']':'['}
+
+        for c in s:
+            if c not in matches: stack.append(c)
+            elif not stack: return False
+            elif stack.pop() != matches[c]: return False
+
+        return not stack
