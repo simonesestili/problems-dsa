9e1d0dc2313dca556316fefd281db997568ed7a0
unknown <yolodoggy911@gmail.com>
2022-04-03 14:16:38 -0700

    31

---
+++ 31.py
@@ -0,0 +1,22 @@
+class Solution:
+    # 1) Find largest index k such that there is some j > k such that nums[j] > nums[k]
+    # 2) Swap nums[k] and nums[-1]
+    # 3) Reverse nums[k + 1:]
+    def nextPermutation(self, nums):
+        n = len(nums)
+
+        def reverse(i, j):
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i, j = i + 1, j - 1
+        
+        for k in range(n - 2, -1, -1):
+            if nums[k] < nums[k+1]: break
+        else:
+            reverse(0, n - 1)
+            return
+
+        j = n - 1
+        while nums[j] <= nums[k]: j -= 1
+        nums[k], nums[j] = nums[j], nums[k]
+        reverse(k + 1, n - 1)
