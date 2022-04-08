b6700a91dec433bf08f6004b80a48e04a41380ab
unknown <yolodoggy911@gmail.com>
2022-04-08 13:18:47 -0700

    1553

--- 1553.py
+++ 1553.py
@@ -2,6 +2,5 @@ class Solution:
     @cache
     def minDays(self, n):
         if n < 3: return n
-        cand1, cand2 = n % 2 + self.minDays(n // 2), n % 3 + self.minDays(n // 3)
-        return 1 + min(cand1, cand2)
+        return 1 + min(n % 3 + self.minDays(n // 3), n % 2 + self.minDays(n // 2))
 
