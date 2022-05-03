09181428f2596f86d162315312ea321ef0fe4028
unknown <yolodoggy911@gmail.com>
2022-05-02 17:42:25 -0700

    581

--- 581.py
+++ 581.py
@@ -1,16 +1,18 @@
 class Solution:     
-    def findUnsortedSubarray(self, nums: List[int]) -> int: 
+    def findUnsortedSubarray(self, nums):
         n = len(nums)
-        asc = sorted(nums)
-        if asc == nums:
-            return 0
-        first, last = -1, -1
+        max_prefix, min_suffix = [float('-inf')], [float('inf')]
+
         for i in range(n):
-            if asc[i] != nums[i]:
-                first = i
-                break
-        for i in range(n - 1, -1, -1):
-            if asc[i] != nums[i]:
-                last = i
-                break
-        return last - first + 1
+            max_prefix.append(max(max_prefix[-1], nums[i]))
+            min_suffix.append(min(min_suffix[-1], nums[-1-i]))
+        min_suffix = min_suffix[::-1]
+
+        left, right = 0, n - 1
+        while left < n - 1 and max_prefix[left+1] <= min_suffix[left+1]:
+            left += 1
+        while right > 0 and min_suffix[right] >= max_prefix[right]:
+            right -= 1
+        
+        size = right - left + 1
+        return 0 if size <= 1 else size
