956c20304190bf16810e546c5eb998d7975dfbd6
Simone <yolodoggy911@gmail.com>
2022-05-09 21:57:17 -0700

    216

--- 216.py
+++ 216.py
@@ -1,16 +1,16 @@
 class Solution:
     def combinationSum3(self, k, n):
-        self.combs = []
+        self.combs, self.curr, = [], []
 
-        def dfs(curr, curr_sum):
-            if len(curr) == k and curr_sum == n:
-                self.combs.append(curr[:])
+        def dfs(curr_sum=0):
+            if len(self.curr) == k and curr_sum == n:
+                self.combs.append(self.curr[:])
                 return
-            for i in range(1 if not curr else curr[-1] + 1, 10):
-                if i not in curr:
-                    curr.append(i)
-                    dfs(curr, curr_sum + i)
-                    curr.pop()
+            for i in range(1 if not self.curr else self.curr[-1] + 1, 10):
+                if curr_sum + i > n: break
+                self.curr.append(i)
+                dfs(curr_sum + i)
+                self.curr.pop()
 
-        dfs([], 0)
+        dfs()
         return self.combs
