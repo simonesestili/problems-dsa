aff55d1e750cb116093f451f213e366261fba241
unknown <yolodoggy911@gmail.com>
2022-05-13 19:33:57 -0700

    743

--- 743.py
+++ 743.py
@@ -1,19 +1,19 @@
 class Solution:
     def networkDelayTime(self, times, n, k):
-        graph, dist = defaultdict(list), [float('inf')] * (n + 1)
+        graph, dist = defaultdict(dict), [float('inf')] * (n + 1)
         dist[0] = dist[k] = 0
 
-        for u, v, w in times: graph[u].append((v, w))
+        for u, v, w in times: graph[u][v] = w
 
         heap = [(dist[i], i) for i in range(1, n + 1)]
         heapify(heap)
 
         while heap:
             time, node = heappop(heap)
-            for to, weight in graph[node]:
-                if time + weight < dist[to]:
-                    heappush(heap, (time + weight, to))
-                    dist[to] = time + weight
+            for neigh, delay in graph[node].items():
+                if dist[neigh] <= time + delay: continue
+                heappush(heap, (time + delay, neigh))
+                dist[neigh] = time + delay
 
         ans = max(dist)
         return -1 if ans == float('inf') else ans
