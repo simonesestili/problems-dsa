ecadbb36397b6c20a2be8181840e7979186b006a
unknown <yolodoggy911@gmail.com>
2022-05-20 17:32:44 -0700

    322

--- 322.py
+++ 322.py
@@ -1,11 +1,10 @@
 class Solution:
     def coinChange(self, coins, amount):
-        n = len(coins)
-        dp = [[0 if not j else float('inf') for j in range(amount + 1)] for i in range(n)]
-        for i in range(n - 1, -1, -1):
-            for j in range(1, amount + 1):
-                diff = j - coins[i]
-                left = float('inf') if diff < 0 else 1 + dp[i][diff]
-                dp[i][j] = min(left, float('inf') if i == n - 1 else dp[i + 1][j])
-        ans = dp[0][-1]
-        return -1 if ans == float('inf') else ans
+        MX = float('inf')
+        dp = [0] + [MX] * amount
+
+        for rem in range(1, amount + 1):
+            dp[rem] = 1 + min((dp[rem - coin] for coin in coins if rem - coin >= 0), default=MX)
+
+        ans = dp[amount]
+        return ans if ans != MX else -1
