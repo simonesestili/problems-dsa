d25cf29329d9910354ef1ded5d71f48109407b70
Simone <yolodoggy911@gmail.com>
2022-07-13 14:27:07 -0700

    102

--- 102.py
+++ 102.py
@@ -1,24 +1,14 @@
 class Solution:
-    def levelOrder(self, root: TreeNode) -> List[List[int]]:
-        if not root:
-            return []
-        levels = []
-        curr_lvl = [root]
-        while curr_lvl:
-            levels.append([node.val for node in curr_lvl])
-            children = []
-            for node in curr_lvl:
-                if node.left:
-                    children.append(node.left)
-                if node.right:
-                    children.append(node.right)
-            curr_lvl = children
-        return levels
+    def levelOrder(self, root):
+        ans, level = [], [root] if root else []
 
+        while level:
+            ans.append([])
+            nxtlvl = []
+            for node in level:
+                if node.left: nxtlvl.append(node.left)
+                if node.right: nxtlvl.append(node.right)
+                ans[-1].append(node.val)
+            level = nxtlvl
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+        return ans
