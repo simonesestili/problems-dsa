3c482d8b1e27b972f62d43f4ed07893da6a990d1
Simone <yolodoggy911@gmail.com>
2022-07-23 23:34:17 -0700

    2354

--- 2354.py
+++ 2354.py
@@ -2,5 +2,4 @@ class Solution:
     def countExcellentPairs(self, nums, k):
         nums, counts = set(nums), defaultdict(int)
         for num in nums: counts[num.bit_count()] += 1
-
-        return sum(sum(counts[i] for i in range(k - num.bit_count(), 32)) for num in nums)
+        return sum(sum(counts[i] for i in range(k - num.bit_count(), 30)) for num in nums)
