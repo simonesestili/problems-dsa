d49cbb873a9f48baf1fc5bfa2716682a945a2fd1
unknown <yolodoggy911@gmail.com>
2022-08-07 12:36:48 -0700

    1220

--- 1220.py
+++ 1220.py
@@ -1,13 +1,12 @@
+MOD = 10 ** 9 + 7
 class Solution:
     def countVowelPermutation(self, n):
-        M = 10 ** 9 + 7
         a = e = i = o = u = 1
         for _ in range(n - 1):
-            A = (e + u + i) % M
-            E = (a + i) % M
-            I = (e + o) % M
-            O = (i) % M
-            U = (o + i) % M
+            A = (e + i + u) % MOD
+            E = (a + i) % MOD
+            I = (e + o) % MOD
+            O = (i) % MOD
+            U = (i + o) % MOD
             a, e, i, o, u = A, E, I, O, U
-
-        return sum([a, e, i, o, u]) % M
+        return sum((a, e, i, o, u)) % MOD
