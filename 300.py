fba1061c10ceaaa6a6de3bd2831fc490265bf4b8
unknown <yolodoggy911@gmail.com>
2022-08-07 18:15:00 -0700

    300

--- 300.py
+++ 300.py
@@ -11,11 +11,11 @@ class Solution:
         return len(piles)
 
 # O(n^2) solution
-# class Solution:     
-# 	def lengthOfLIS(self, nums: List[int]) -> int:
-#         subproblems = [1] * len(nums)
-#         for i in range(1, len(nums)):
-#             for j in range(i):
-#                 if nums[i] > nums[j]:
-#                     subproblems[i] = max(subproblems[i], subproblems[j] + 1)
-#         return max(subproblems)
+class Solution:     
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        subproblems = [1] * len(nums)
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[i] <= nums[j]: continue
+                subproblems[i] = max(subproblems[i], subproblems[j] + 1)
+        return max(subproblems)
