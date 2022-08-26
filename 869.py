2630c20b7c2d1f622c49ab9ab6177e0f4fec0ce8
Simone <yolodoggy911@gmail.com>
2022-08-25 19:21:36 -0700

    869

--- 869.py
+++ 869.py
@@ -1,8 +1,8 @@
 class Solution:
     def reorderedPowerOf2(self, n):
-        digs, curr = Counter(str(n)), 1
-        for _ in range(31):
-            if Counter(str(curr)) == digs:
+        target, curr = Counter(str(n)), 1
+        for _ in range(32):
+            if Counter(str(curr)) == target:
                 return True
             curr <<= 1
         return False
