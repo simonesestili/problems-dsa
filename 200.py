55c82fcb9d4b5b93f43cbec9ce962e7fde134210
Simone <yolodoggy911@gmail.com>
2022-08-28 23:05:31 -0700

    200

--- 200.py
+++ 200.py
@@ -1,21 +1,17 @@
+DIRS = ((1, 0), (0, 1), (-1, 0), (0, -1))
 class Solution:
     def numIslands(self, grid):
         m, n = len(grid), len(grid[0])
-        DIRS = ((1,0), (0,1), (-1,0), (0,-1))
+        inbounds = lambda (i, j): 0 <= i < m and 0 <= j < n
 
-        def explore(i, j):
+        def sink(i, j):
+            if not inbounds(i, j) or grid[i][j] == '0': return
             grid[i][j] = '0'
-            for di, dj in DIRS:
-                di, dj = i + di, j + dj
-                if 0 <= di < m and 0 <= dj < n and grid[di][dj] == '1':
-                    explore(di, dj)
+            for I, J in DIRS: sink(i + I, j + J)
 
         ans = 0
-        for row in range(m):
-            for col in range(n):
-                if grid[row][col] == '0': continue
-                explore(row, col)
-                ans += 1
-
+        for i in range(m):
+            for j in range(n):
+                ans += grid[i][j] == '1'
+                sink(i, j)
         return ans
-                
