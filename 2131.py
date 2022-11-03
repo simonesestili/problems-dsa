51b41fd576c63639daf003f62b1de7e2f3ae4e60
Simone <yolodoggy911@gmail.com>
2022-11-03 14:15:58 -0700

    2131

--- 2131.py
+++ 2131.py
@@ -1,11 +1,13 @@
 class Solution:
     def longestPalindrome(self, words):
-        ans, count = 0, Counter(words)
-        mid = 0
-        for word in count:
-            if word[0] == word[1]:
-                ans += count[word] - count[word] % 2
-                mid += count[word] % 2
+        ans = mid = 0
+        seen = defaultdict(int)
+        for word in words:
+            if seen[word[::-1]]:
+                seen[word[::-1]] -= 1
+                mid -= word[0] == word[1]
+                ans += 4
             else:
-                ans += min(count[word], count[word[::-1]])
-        return (ans + min(1, mid)) * 2
+                seen[word] += 1
+                mid += word[0] == word[1]
+        return ans + (2 if mid else 0)
