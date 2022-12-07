4b37e747362842853cda94e80cda5aad5b654965
simonesestili <yolodoggy911@gmail.com>
2022-12-07 12:00:54 -0800

    938

--- 938.py
+++ 938.py
@@ -1,18 +1,10 @@
 class Solution:
     def rangeSumBST(self, root, lo, hi):
-        self.total = 0
-
+        
         def dfs(node):
-            if not node: 
-                return
-            if lo <= node.val <= hi:
-                self.total += node.val
-                dfs(node.left)
-                dfs(node.right)
-            elif node.val < lo:
-                dfs(node.right)
-            else:
-                dfs(node.left)
+            if not node: return 0
+            if node.val < lo: return dfs(node.right)
+            if node.val > hi: return dfs(node.left)
+            return node.val + dfs(node.left) + dfs(node.right)
 
-        dfs(root)
-        return self.total
+        return dfs(root)
