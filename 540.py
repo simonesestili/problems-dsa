a7a83c44169f77d3fa4bdee52d32f97b40cbc0ec
simonesestili <yolodoggy911@gmail.com>
2023-02-20 17:10:52 -0800

    540

--- 540.py
+++ 540.py
@@ -1,20 +1,10 @@
 class Solution:
     def singleNonDuplicate(self, nums):
-        n = len(nums)
-        left, right = 0, n - 1
+        left, right = 0, len(nums) - 1
         while left < right:
-            mid = (left + right) // 2
-            if nums[mid - 1] == nums[mid]:
-                if (right - mid) % 2:
-                    left = mid + 1
-                else:
-                    right = mid - 2
-            elif nums[mid] == nums[mid + 1]:
-                if (right - mid - 1) % 2:
-                    left = mid + 2
-                else:
-                    right = mid - 1
+            cand = (left + right) // 2
+            if nums[cand - cand % 2] == nums[cand - cand % 2 + 1]:
+                left = cand - cand % 2 + 2
             else:
-                return nums[mid]
-
+                right = cand
         return nums[left]
