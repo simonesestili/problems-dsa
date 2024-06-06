4fb41043d3aea77d50894afdae6de47c57eb40dd
simonesestili <yolodoggy911@gmail.com>
2024-06-06 07:57:39 -0700

    846

--- 846.py
+++ 846.py
@@ -1,13 +1,14 @@
 class Solution:
     def isNStraightHand(self, hand, size):
-        n, count = len(hand), Counter(hand)
-        if n % size: return False
+        if len(hand) % size: return False
 
-        for val in sorted(count.keys()):
-            if count[val-1] > 0 or count[val] == 0: continue
-            c = count[val]
-            for x in range(val, val + size):
-                if c > count[x]: return False
-                count[x] -= c
+        counts = Counter(hand)
+        for val in sorted(counts.keys()):
+            if not counts[val]: continue
+            c = counts[val]
+            for i in range(size):
+                if c > counts[val+i]: return False
+                counts[val+i] -= c
 
-        return not any(count.values())
+        return True
+
