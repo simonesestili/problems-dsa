97ccadb482244a75334ca42a64daaddb0a5c2179
simonesestili <yolodoggy911@gmail.com>
2024-06-08 01:17:13 -0400

    523

--- 523.py
+++ 523.py
@@ -1,10 +1,11 @@
 class Solution:
     def checkSubarraySum(self, nums, k):
         prefix, curr = {0: -1}, 0
-        for i, num in enumerate(nums):
-            curr = (curr + num) % k
+        for i, x in enumerate(nums):
+            curr = (curr + x) % k
             if i - prefix.get(curr, i) >= 2:
                 return True
-            if curr % k not in prefix:
-                prefix[curr % k] = i
+            if curr not in prefix:
+                prefix[curr] = i
         return False
+
