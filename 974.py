86670302c6f34fe5f0da1e3a863a7eb6e31a6dda
simonesestili <yolodoggy911@gmail.com>
2024-06-09 15:28:07 -0400

    974

--- 974.py
+++ 974.py
@@ -1,9 +1,11 @@
 class Solution:
     def subarraysDivByK(self, nums, k):
         ans = curr = 0
-        seen = defaultdict(int); seen[0] = 1
+        prefix = defaultdict(int)
+        prefix[0] = 1
         for x in nums:
             curr = (curr + x) % k
-            ans += seen[curr]
-            seen[curr] += 1
+            ans += prefix[curr]
+            prefix[curr] += 1
         return ans
+
