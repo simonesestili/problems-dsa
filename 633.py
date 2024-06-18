de3bb35ed33d8168d24bdbe030c554183ebfc157
simonesestili <yolodoggy911@gmail.com>
2024-06-17 21:54:26 -0700

    633 826

--- 633.py
+++ 633.py
@@ -1,6 +1,7 @@
 class Solution:
-    def judgeSquareSum(self, c: int) -> bool:
-        for i in range(int(math.sqrt(c)) + 1):
-            if math.sqrt(c - i ** 2).is_integer():
-                return True
+    def judgeSquareSum(self, c):
+        for b in range(int(math.sqrt(c)) + 1):
+            if b ** 2 > c: break
+            if math.sqrt(c - b ** 2).is_integer(): return True
         return False
+
--- 826.py
+++ 826.py
@@ -1,13 +1,11 @@
 class Solution:
     def maxProfitAssignment(self, difficulty, profit, worker):
-        jobs = []
-        for d, p in zip(difficulty, profit): jobs.append([d, p])
-        jobs.sort()
-        for i in range(1, len(jobs)): jobs[i][1] = max(jobs[i-1][1], jobs[i][1])
-
-        ans = 0
-        for skill in worker:
-            i = bisect_right(jobs, [skill, float('inf')]) - 1
-            ans += 0 if i == -1 else jobs[i][1]
-        return ans
+        jobs, worker = sorted(zip(profit, difficulty)), sorted(worker)
+        profit = 0
+        while worker:
+            while jobs and jobs[-1][1] > worker[-1]: jobs.pop()
+            if not jobs: break
+            profit += jobs[-1][0]
+            worker.pop()
+        return profit
 
