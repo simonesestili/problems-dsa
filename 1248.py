4420ed8f272db9e3a944414c5f2b64a420ec0b10
simonesestili <yolodoggy911@gmail.com>
2024-06-21 17:42:18 -0700

    1248

--- 1248.py
+++ 1248.py
@@ -1,7 +1,6 @@
 class Solution:
     def numberOfSubarrays(self, nums, k):
-        ans, curr, prefix = 0, 0, defaultdict(int)
-        prefix[0] = 1
+        ans, curr, prefix = 0, 0, defaultdict(int, {0: 1})
         for x in nums:
             curr += x % 2
             ans += prefix[curr - k]
