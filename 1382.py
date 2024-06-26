2a2d9d649e7f52f3518746d9c945eea5ff65c5e1
simonesestili <yolodoggy911@gmail.com>
2024-06-25 20:01:53 -0700

    1382

--- 1382.py
+++ 1382.py
@@ -1,17 +1,17 @@
 class Solution:
     def balanceBST(self, root):
-        self.arr = []
+        vals = []
 
         def dfs(node):
-            if not node: return
+            if node is None: return
             dfs(node.left)
-            self.arr.append(node.val)
+            vals.append(node.val)
             dfs(node.right)
 
-        def build(i, j):
-            if i > j: return None
-            mid = (i + j) // 2
-            return TreeNode(self.arr[mid], build(i, mid - 1), build(mid + 1, j))
+        def build(l, r):
+            if l > r: return None
+            mid = (l + r) // 2
+            return TreeNode(vals[mid], build(l, mid - 1), build(mid + 1, r))
 
         dfs(root)
-        return build(0, len(self.arr) - 1)
+        return build(0, len(vals) - 1)
