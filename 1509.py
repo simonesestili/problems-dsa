2247e91dada5a353f74f9b8f694b8f1539f879b0
simonesestili <yolodoggy911@gmail.com>
2024-07-02 19:23:08 -0700

    1509

--- 1509.py
+++ 1509.py
@@ -1,7 +1,6 @@
 class Solution:
     def minDifference(self, nums):
-        n = len(nums)
-        if n <= 3:
-            return 0
+        if len(nums) <= 4: return 0
         nums.sort()
-        return min([nums[-4] - nums[0], nums[-3] - nums[1], nums[-2] - nums[2], nums[-1] - nums[3]])
+        return min(nums[-1] - nums[3], nums[-2] - nums[2], nums[-3] - nums[1], nums[-4] - nums[0])
+
