a926b4dc05d6057ab172c8e7647df434dd07a231
simonesestili <yolodoggy911@gmail.com>
2024-07-28 19:11:45 -0700

    1395

--- 1395.py
+++ 1395.py
@@ -1,19 +1,11 @@
+from sortedcontainers import SortedList
 class Solution:
     def numTeams(self, rating):
-        ans, n = 0, len(rating)
-        right, left = [0] * n, [0] * n
-
-        for i in range(n):
-            for j in range(i): left[i] += rating[j] > rating[i]
-            for j in range(i, n): right[i] += rating[j] > rating[i]
-
-        for i in range(n):
-            for j in range(i + 1, n):
-                if rating[i] < rating[j]:
-                    ans += right[j]
-        for i in range(n - 1, -1, -1):
-            for j in range(i - 1, -1, -1):
-                if rating[i] < rating[j]:
-                    ans += left[j]
-
+        ans, n, left, right = 0, len(rating), SortedList(), SortedList(rating)
+        for i, x in enumerate(rating):
+            right.remove(x)
+            ans += left.bisect_left(x) * (n - i - 1 - right.bisect_right(x))
+            ans += (i - left.bisect_right(x)) * right.bisect_left(x)
+            left.add(x)
         return ans
+
