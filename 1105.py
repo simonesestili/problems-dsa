5c649bcd37acb1b9be07ce7d72c8b70b49ffe154
simonesestili <yolodoggy911@gmail.com>
2024-07-31 01:52:40 -0700

    1105 1653

--- 1105.py
+++ 1105.py
@@ -1,15 +1,15 @@
 class Solution:
     def minHeightShelves(self, books, width):
         n = len(books)
-        dp = [float('inf')] * (n + 1)
-        dp[-1] = 0
+        dp = [inf] * n + [0]
 
         for i in range(n - 1, -1, -1):
-            h = w = 0
+            h, w = 0, width
             for j in range(i, n):
                 h = max(h, books[j][1])
-                w += books[j][0]
-                if w > width: break
+                w -= books[j][0]
+                if w < 0: break
                 dp[i] = min(dp[i], h + dp[j + 1])
 
         return dp[0]
+
--- 1653.py
+++ 1653.py
@@ -1,15 +1,10 @@
 class Solution:     
-    def minimumDeletions(self, s: str) -> int: 
-        n = len(s)
-        aDeletions, aCount = [0] * n, 0
-        for i in range(n - 1, -1, -1):
-            if s[i] == 'a':
-                aCount += 1
-            aDeletions[i] = aCount    
-        least = aDeletions[0]
-        bCount = 0
-        for i in range(n):
-            least = min(least, bCount + aDeletions[i])
-            if s[i] == 'b':
-                bCount += 1
-        return min(least, bCount) 
+    def minimumDeletions(self, s):
+        arem, brem = s.count('a'), 0
+        ans = arem
+        for c in s:
+            if c == 'a': arem -= 1
+            else: brem += 1
+            ans = min(ans, arem + brem)
+        return ans
+
