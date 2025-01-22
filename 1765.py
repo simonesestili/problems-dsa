f26ce8b72e68b59f7c7c7e5626c67135d3460b7a
simonesestili <yolodoggy911@gmail.com>
2025-01-21 18:14:27 -0800

    1765

--- 1765.py
+++ 1765.py
@@ -1,24 +1,25 @@
+DIRS = ((1, 0), (0, 1), (-1, 0), (0, -1))
 class Solution:
-    def highestPeak(self, isWater):
-        m, n = len(isWater), len(isWater[0])
-        ans, curr = [[-1] * n for _ in range(m)], []
-        for row in range(m):
-            for col in range(n):
-                if isWater[row][col]:
-                    curr.append((row, col))
+    def highestPeak(self, grid):
+        m, n, q, seen = len(grid), len(grid[0]), deque(), set()
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c]:
+                    grid[r][c] = 0
+                    q.append((r, c, 0))
+                    seen.add((r, c))
 
-        DIRS = ((1,0), (0,1), (-1,0), (0,-1))
-        height = 0
-        while curr:
-            next_level = []
-            for row, col in curr:
-                if ans[row][col] != -1: continue
-                ans[row][col] = height
-                for drow, dcol in DIRS:
-                    drow, dcol = row + drow, col + dcol
-                    if drow >= 0 and dcol >= 0 and drow < m and dcol < n:
-                        next_level.append((drow, dcol))
-            curr = next_level
-            height += 1
+        while q:
+            r, c, h = q.popleft()
+            for dr, dc in DIRS:
+                dr, dc = r + dr, c + dc
+                if 0 <= dr < m and 0 <= dc < n and (dr, dc) not in seen:
+                    grid[dr][dc] = h + 1
+                    q.append((dr, dc, h + 1))
+                    seen.add((dr, dc))
 
-        return ans
+        for r in range(m):
+            for c in range(n):
+                grid[r][c] = max(grid[r][c], 0)
+
+        return grid
