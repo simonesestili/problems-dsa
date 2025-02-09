a1884e935adb099ce829e7d6a45bd12db98bdf21
simonesestili <yolodoggy911@gmail.com>
2025-02-08 19:33:44 -0800

    2364

--- leetcode/2364.py
+++ leetcode/2364.py
@@ -1,8 +1,7 @@
 class Solution:
     def countBadPairs(self, nums):
-        n, valid = len(nums), 0
-        seen = defaultdict(int)
+        ans, seen = len(nums) * (len(nums) - 1) // 2, defaultdict(int)
         for i, x in enumerate(nums):
-            valid += seen[i - x]
-            seen[i - x] += 1
-        return n * (n - 1) // 2 - valid
+            ans -= seen[x-i]
+            seen[x-i] += 1
+        return ans
