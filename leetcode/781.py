121ee9ba258d7126c33a1648e87c1919d5ea0ad4
simonesestili <yolodoggy911@gmail.com>
2025-04-19 17:25:00 -0700

    781

--- leetcode/781.py
+++ leetcode/781.py
@@ -1,12 +1,8 @@
 class Solution:
-    def numRabbits(self, answers: List[int]) -> int:
-        groups = defaultdict(int)
-        for ans in answers:
-            groups[ans + 1] += 1
-        num_rabbits = 0
-        for group in groups:
-            if groups[group] % group == 0:
-                num_rabbits += groups[group]
-            else:
-                num_rabbits += groups[group] + (group - groups[group] % group)
-        return num_rabbits        
+    def numRabbits(self, answers):
+        ans, groups = 0, defaultdict(int)
+        for cnt in answers:
+            if groups[cnt + 1] % (cnt + 1) == 0:
+                ans += cnt + 1
+            groups[cnt + 1] = (groups[cnt + 1] + 1) % (cnt + 1)
+        return ans
