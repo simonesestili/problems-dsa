620ffa25d969f850ea810600de9c39aafaf8d766
simonesestili <yolodoggy911@gmail.com>
2025-04-27 17:42:12 -0700

    2302

--- leetcode/2302.py
+++ leetcode/2302.py
@@ -1,18 +1,13 @@
 class Solution:
     def countSubarrays(self, nums, k):
-        n, prefix = len(nums), list(accumulate(nums))
-        ans = 0
+        ans = left = curr = 0
 
-        for i, x in enumerate(nums):
-            if x >= k: continue
-            lo, hi = i, n - 1
-            while lo < hi:
-                mid = (lo + hi + 1) // 2
-                val = (prefix[mid] - (0 if not i else prefix[i-1])) * (mid - i + 1)
-                if val >= k: hi = mid - 1
-                else: lo = mid
-            ans += lo - i + 1
+        for right, x in enumerate(nums):
+            curr += x
+            while curr * (right - left + 1) >= k:
+                curr -= nums[left]
+                left += 1
+            if curr * (right - left + 1) < k:
+                ans += right - left + 1
 
         return ans
-
-
