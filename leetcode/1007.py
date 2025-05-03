ea308ce75d5616ed65b535b84092d5a54601f6ca
simonesestili <yolodoggy911@gmail.com>
2025-05-03 14:43:25 -0700

    1007

--- leetcode/1007.py
+++ leetcode/1007.py
@@ -1,15 +1,9 @@
 class Solution:
-    def minDominoRotations(self, a, b):
+    def minDominoRotations(self, tops, bottoms):
+        def solve(val):
+            if any(val not in pair for pair in zip(tops, bottoms)):
+                return inf
+            return len(tops) - max(tops.count(val), bottoms.count(val))
 
-        def solve(main, aux, target):
-            moves = 0
-            for i in range(len(main)):
-                if main[i] == target: continue
-                elif aux[i] == target: moves += 1
-                else: return float('inf')
-            return moves
-
-        fulltop = min(solve(a, b, a[0]), solve(a, b, b[0]))
-        fullbot = min(solve(b, a, a[0]), solve(b, a, b[0]))
-        ans = min(fulltop, fullbot)
-        return -1 if ans == float('inf') else ans
+        ans = min(solve(tops[0]), solve(bottoms[0]))
+        return ans if ans != inf else -1
