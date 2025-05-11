c5005626dd35adac103da34540b107f4272c182f
simonesestili <yolodoggy911@gmail.com>
2025-05-10 17:05:53 -0700

    1550

--- leetcode/1550.py
+++ leetcode/1550.py
@@ -1,4 +1,3 @@
 class Solution:
     def threeConsecutiveOdds(self, arr):
-        return any(arr[i-1] % 2 == arr[i] % 2 == arr[i+1] % 2 == 1 for i in range(1, len(arr) - 1))
-
+        return any(arr[i-1] & arr[i] & arr[i+1] & 1 for i in range(1, len(arr) - 1))
