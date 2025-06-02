c719d84965fdee30e692dc5d78ebf8cfa4e87092
simonesestili <yolodoggy911@gmail.com>
2025-06-02 12:02:26 -0700

    135

--- leetcode/135.py
+++ leetcode/135.py
@@ -1,12 +1,12 @@
 class Solution:
     def candy(self, ratings):
         n = len(ratings)
-        candies = [1] * n
 
-        for rating, i in sorted((ratings[i], i) for i in range(n)):
-            if i and ratings[i-1] < rating:
-                candies[i] = max(candies[i], candies[i-1] + 1)
-            if i != n - 1 and ratings[i+1] < rating:
-                candies[i] = max(candies[i], candies[i+1] + 1)
+        ans = [1] * n
+        for rating, i in sorted((x, i) for i, x in enumerate(ratings)):
+            if i > 0 and ratings[i] > ratings[i-1]:
+                ans[i] = max(ans[i], ans[i-1] + 1)
+            if i < n - 1 and ratings[i] > ratings[i+1]:
+                ans[i] = max(ans[i], ans[i+1] + 1)
 
-        return sum(candies)
+        return sum(ans)
