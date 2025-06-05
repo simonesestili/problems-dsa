eb35068204700a389a15bf8f4837d89f0996a0d4
simonesestili <yolodoggy911@gmail.com>
2025-06-05 16:00:06 -0700

    1061

--- leetcode/1061.py
+++ leetcode/1061.py
@@ -1,18 +1,27 @@
 class Solution:
     def smallestEquivalentString(self, s1, s2, base):
-        uf, code = UF(26), lambda c: ord(c) - ord('a')
-        for a, b in zip(s1, s2): uf.union(code(a), code(b))
-        return ''.join((chr(ord('a') + min(i for i in range(26) if uf.find(i) == uf.find(code(c)))) for c in base))
+        dsu, code = DSU(26), lambda c: ord(c) - ord('a')
+        for a, b in zip(s1, s2): dsu.union(code(a), code(b))
+        return ''.join(chr(ord('a') + min(i for i in range(26) if dsu.find(i) == dsu.find(code(c)))) for c in base)
 
-class UF:
+
+class DSU:
     def __init__(self, n):
-        self.parent = list(range(n))
+        self.reps = list(range(n))
+        self.ranks = [1] * n
 
     def find(self, node):
-        while self.parent[node] != node:
-            node = self.parent[node]
+        while self.reps[node] != node:
+            node = self.reps[node]
         return node
 
     def union(self, a, b):
         ap, bp = self.find(a), self.find(b)
-        if ap != bp: self.parent[ap] = bp
+        if ap == bp: return
+
+        if self.ranks[ap] > self.ranks[bp]:
+            self.reps[bp] = ap
+            self.ranks[ap] += self.ranks[bp]
+        else:
+            self.reps[ap] = bp
+            self.ranks[bp] += self.ranks[ap]
