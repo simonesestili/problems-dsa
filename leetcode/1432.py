1021ce0c9392e4c6f6f054ff8136c3b48ee821d8
simonesestili <yolodoggy911@gmail.com>
2025-06-14 19:43:49 -0700

    1432

--- leetcode/1432.py
+++ leetcode/1432.py
@@ -1,18 +1,11 @@
 class Solution:
     def maxDiff(self, num):
+        num = str(num)
 
-        def maximize(val):
-            for d in val:
-                if d != '9':
-                    return int(val.replace(d, '9'))
-            return int(val)
+        x = next(filter(lambda d: d != '9', num), 'x')
+        mx = num.replace(x, '9')
 
-        def minimize(val):
-            for d in val:
-                if d != '0' and val[0] != d:
-                    return int(val.replace(d, '0'))
-                if d not in '01':
-                    return int(val.replace(d, '1'))
-            return int(val)
+        x = next(filter(lambda d: d != '0' and (d != '1' or num[0] != '1'), num), 'x')
+        mn = num.replace(x, '1' if x == num[0] else '0')
 
-        return maximize(str(num)) - minimize(str(num))
+        return int(mx) - int(mn)
