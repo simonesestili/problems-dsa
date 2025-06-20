5efb58f6b4020d30fd703b4743ae2a044708e861
simonesestili <yolodoggy911@gmail.com>
2025-06-19 17:53:30 -0700

    3443

--- leetcode/3443.py
+++ leetcode/3443.py
@@ -3,14 +3,10 @@ class Solution:
         ans = x = y = 0
         cnts = defaultdict(int)
         for c in s:
-            if s == 'N': y += 1
-            elif s == 'S': y -= 1
-            elif s == 'E': x += 1
-            elif s == 'W': x -= 1
             cnts[c] += 1
-            ans = max(ans, abs(x) + abs(y) + 2 * min(k, min(cnts['N'], cnts['S']) + min(cnts['W'], cnts['E'])))
+            if c == 'N': y += 1
+            elif c == 'S': y -= 1
+            elif c == 'E': x += 1
+            elif c == 'W': x -= 1
+            ans = max(ans, abs(x) + abs(y) + 2 * min(k, min(cnts['N'], cnts['S']) + min(cnts['E'], cnts['W'])))
         return ans
-
-
-
-
