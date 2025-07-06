d228ceffa70c9db05f16c50610b929c5b9517cdb
simonesestili <yolodoggy911@gmail.com>
2025-07-05 22:55:46 -0400

    1865

--- leetcode/1865.py
+++ leetcode/1865.py
@@ -2,16 +2,12 @@ class FindSumPairs:
     def __init__(self, nums1, nums2):
         self.nums1 = nums1
         self.nums2 = nums2
-        self.freq = Counter(nums2)
+        self.cnts = Counter(nums2)
 
-    def add(self, idx, val):
-        num = self.nums2[idx]
-        self.freq[num] -= 1
-        self.freq[num + val] += 1
-        self.nums2[idx] += val
+    def add(self, i, val):
+        self.cnts[self.nums2[i]] -= 1
+        self.nums2[i] += val
+        self.cnts[self.nums2[i]] += 1
 
     def count(self, tot):
-        count = 0
-        for num in self.nums1:
-            count += self.freq[tot - num]
-        return count
+        return sum(self.cnts[tot - x] for x in self.nums1)
