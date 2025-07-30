c8cd8f6ad0b123a50d415a5a330aec0594a9cd36
simonesestili <yolodoggy911@gmail.com>
2025-07-29 20:43:02 -0400

    2419

--- leetcode/2419.py
+++ leetcode/2419.py
@@ -1,9 +1,8 @@
 class Solution:
     def longestSubarray(self, nums):
-        ans, curr, mx = 1, 0, max(nums)
+        ans, curr, mx = 0, 0, max(nums)
         for x in nums:
             if x == mx: curr += 1
             else: curr = 0
             ans = max(ans, curr)
         return ans
-
