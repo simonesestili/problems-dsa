4eaa5b672ca9679f2f5fb610e35852b2ac8bc2e9
simonesestili <yolodoggy911@gmail.com>
2025-08-13 23:53:47 -0400

    2264

--- leetcode/2264.py
+++ leetcode/2264.py
@@ -1,7 +1,3 @@
 class Solution:
     def largestGoodInteger(self, num):
-        for i in range(9, -1, -1):
-            cand = str(i) * 3
-            if cand in num:
-                return cand
-        return ''
+        return max((num[i:i+3] for i in range(len(num) - 2) if num[i] == num[i+1] == num[i+2]), default='')
