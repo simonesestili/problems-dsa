33d4c9d22eb2a4dd2af7f56430f51f0141695d6f
simonesestili <yolodoggy911@gmail.com>
2025-08-25 15:27:46 -0400

    498

--- leetcode/498.py
+++ leetcode/498.py
@@ -1,27 +1,22 @@
 class Solution:
-	def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-		flattened = []	
-		row, col = 0, 0
-		m, n = len(mat), len(mat[0])
-		dir = [-1, 1]
-
-		while len(flattened) < m * n:
-			flattened.append(mat[row][col])
-			row, col = row + dir[0], col + dir[1]
-			if col == n:
-				row += 2
-				col -= 1
-				dir = [1, -1]
-			elif row == -1:
-				row += 1
-				dir = [1, -1]
-			elif row == m:
-				row -= 1
-				col += 2
-				dir = [-1, 1]
-			elif col == -1:
-				col += 1
-				dir = [-1, 1]
-			
-		return flattened
-
+    def findDiagonalOrder(self, mat):
+        m, n = len(mat), len(mat[0])
+        ans, r, c, d = [], 0, 0, 1
+        for _ in range(m*n):
+            ans.append(mat[r][c])
+            r, c = r-d, c+d
+            if c == n:
+                r += 2
+                c -= 1
+                d *= -1
+            elif r == -1:
+                r += 1
+                d *= -1
+            elif r == m:
+                r -= 1
+                c += 2
+                d *= -1
+            elif c == -1:
+                c += 1
+                d *= -1
+        return ans
