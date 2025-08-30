ad6f84737c5269c301c0c2e3255ea7700326d53c
simonesestili <yolodoggy911@gmail.com>
2025-08-29 21:04:49 -0400

    36

--- leetcode/36.py
+++ leetcode/36.py
@@ -1,5 +1,10 @@
 class Solution:
     def isValidSudoku(self, board):
-        for row in range(9):
-            seen = set()
-
+        for i in range(9):
+            row = [board[i][c] for c in range(9) if board[i][c] != '.']
+            col = [board[r][i] for r in range(9) if board[r][i] != '.']
+            box = [board[i//3*3+j//3][i%3*3+j%3] for j in range(9) if board[i//3*3+j//3][i%3*3+j%3] != '.']
+            if len(row) != len(set(row)): return False
+            if len(col) != len(set(col)): return False
+            if len(box) != len(set(box)): return False
+        return True
