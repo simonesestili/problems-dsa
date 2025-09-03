029a6abff743fae8171f806791fa3dda7d639d1a
simonesestili <yolodoggy911@gmail.com>
2025-09-03 10:29:01 -0400

    3027

---
+++ leetcode/3027.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numberOfPairs(self, points):
+        ans, points = 0, sorted(points, key=lambda p: (p[0], -p[1]))
+        for i in range(len(points)):
+            prev = -inf
+            for j in range(i + 1, len(points)):
+                if prev < points[j][1] <= points[i][1]:
+                    prev = points[j][1]
+                    ans += 1
+        return ans
