3a2499e53140cc2d1162f8cbe54470bc4a021f9f
simonesestili <yolodoggy911@gmail.com>
2025-09-08 10:43:07 -0400

    1317

--- leetcode/1317.py
+++ leetcode/1317.py
@@ -1,7 +1,19 @@
 class Solution:
     def getNoZeroIntegers(self, n):
-        for i in range(1, n // 2 + 1):
-            A, B = i, n - i
-            if not str(A).count('0') + str(B).count('0'):
-                return [A, B]
-        return None
+        ans, p = [0, 0], 1
+        while n:
+            n, r = divmod(n, 10)
+            if r == 0:
+                ans[0] += 5 * p
+                ans[1] += 5 * p
+                n -= 1
+            elif r == 1 and n:
+                ans[0] += 6 * p
+                ans[1] += 5 * p
+                n -= 1
+            else:
+                ans[0] += (r-r//2) * p
+                ans[1] += (r//2) * p
+            p *= 10
+        return ans
+
