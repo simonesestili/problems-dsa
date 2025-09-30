22078e66055b0953eccb99e25088e796b7efe7f5
simonesestili <yolodoggy911@gmail.com>
2025-09-30 14:43:06 -0400

    2221

--- leetcode/2221.py
+++ leetcode/2221.py
@@ -1,10 +1,4 @@
 class Solution:
     def triangularSum(self, nums):
-
-        while len(nums) > 1:
-            new = [0] * (len(nums) - 1)
-            for i in range(len(new)):
-                new[i] = (nums[i] + nums[i+1]) % 10
-            nums = new
-
-        return nums[0]
+        n = len(nums)
+        return sum(nums[i] * comb(n - 1, i) for i in range(n)) % 10
