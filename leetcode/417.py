764df1f53a92e51189c54694d2244ea21a4b8a55
simonesestili <yolodoggy911@gmail.com>
2025-10-05 11:57:56 -0400

    417

--- leetcode/417.py
+++ leetcode/417.py
@@ -4,17 +4,20 @@ class Solution:
         m, n = len(heights), len(heights[0])
         inbounds = lambda r, c: 0 <= r < m and 0 <= c < n
 
-        def climb(curr):
-            seen = set(curr)
-            while curr:
-                r, c = curr.popleft()
-                for R, C in DIRS:
-                    R, C = r+R, c+C
-                    if not inbounds(R, C) or (R, C) in seen or heights[r][c] > heights[R][C]: continue
-                    curr.append((R, C))
+        def dfs(r, c, seen):
+            for R, C in DIRS:
+                R, C = r+R, c+C
+                if inbounds(R, C) and (R, C) not in seen and heights[R][C] >= heights[r][c]:
                     seen.add((R, C))
-            return seen
+                    dfs(R, C, seen)
 
-        P = deque([(r, 0) for r in range(m)] + [(0, c) for c in range(1, n)])
-        A = deque([(r, n-1) for r in range(m)] + [(m-1, c) for c in range(n-1)])
-        return climb(P) & climb(A)
+        p = set()
+        for r, c in [(r, 0) for r in range(m)] + [(0, c) for c in range(1, n)]:
+            p.add((r, c))
+            dfs(r, c, p)
+        a = set()
+        for r, c in [(r, n-1) for r in range(m)] + [(m-1, c) for c in range(n-1)]:
+            a.add((r, c))
+            dfs(r, c, a)
+
+        return list(p & a)
