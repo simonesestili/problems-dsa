732e8e5d979e2f6f602bc753502bac25f0979b44
simonesestili <yolodoggy911@gmail.com>
2025-10-07 22:07:48 -0400

    2300

--- leetcode/2300.py
+++ leetcode/2300.py
@@ -1,14 +1,4 @@
 class Solution:
     def successfulPairs(self, spells, potions, success):
-        n, m, potions = len(spells), len(potions), sorted(potions)
-
-        def count(sp):
-            lo, hi = 0, m - 1
-            if sp * potions[-1] < success: return 0
-            while lo < hi:
-                mid = (lo + hi) // 2
-                if sp * potions[mid] >= success: hi = mid
-                else: lo = mid + 1
-            return m - lo
-
-        return map(count, spells)
+        potions.sort()
+        return [len(potions) - bisect_left(potions, ceil(success / s)) for s in spells]
