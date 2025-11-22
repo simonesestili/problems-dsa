866fdaa33b199cb14ef3638908adc66fb501af33
simonesestili <yolodoggy911@gmail.com>
2025-11-22 09:34:29 -0500

    1930 3190

--- leetcode/1930.py
+++ leetcode/1930.py
@@ -1,6 +1,5 @@
 class Solution:
     def countPalindromicSubsequence(self, s):
         l, r = {}, {}
-        for i, c in enumerate(s):
-            l[c], r[c] = l.get(c, i), i
+        for i, c in enumerate(s): l[c], r[c] = l.get(c, i), i
         return sum(len(set(s[l[c]+1:r[c]])) for c in l if l[c] < r[c])
