0756f8cbf44ec821ba1f33bbbfb4c39db020938f
simonesestili <yolodoggy911@gmail.com>
2026-01-05 21:04:14 -0800

    1161

--- leetcode/1161.py
+++ leetcode/1161.py
@@ -1,18 +1,13 @@
 class Solution:
     def maxLevelSum(self, root):
-        level, i = [root], 1
-        best, best_idx = float('-inf'), -1
-        while level:
-            next_level = []
-            curr = 0
-            for node in level:
-                curr += node.val
-                if node.left:
-                    next_level.append(node.left)
-                if node.right:
-                    next_level.append(node.right)
-            if curr > best:
-                best, best_idx = curr, i
-            i += 1
-            level = next_level
-        return best_idx
+        ans, mx, curr, i = 1, -inf, [root], 0
+        while curr:
+            s, nxt, i = 0, [], i + 1
+            for node in curr:
+                s += node.val
+                if node.left: nxt.append(node.left)
+                if node.right: nxt.append(node.right)
+            if s > mx:
+                ans, mx = i, s
+            curr = nxt
+        return ans
