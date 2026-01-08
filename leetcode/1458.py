1de06ab98fe938523f625b4c00a9872326b65841
simonesestili <yolodoggy911@gmail.com>
2026-01-07 22:21:23 -0800

    1458

--- leetcode/1458.py
+++ leetcode/1458.py
@@ -1,13 +1,11 @@
 class Solution:
-    def maxDotProduct(self, nums1, nums2):
-        n, m = len(nums1), len(nums2)
+    def maxDotProduct(self, a, b):
+        n, m = len(a), len(b)
 
         @cache
-        def dp(i=0, j=0, empty=True):
-            if i >= n or j >= m: return float('-inf') if empty else 0
-            first = nums1[i] * nums2[j] + dp(i + 1, j + 1, False)
-            second = dp(i + 1, j, empty)
-            third = dp(i, j + 1, empty)
-            return max(first, second, third)
+        def dp(i, j, choose):
+            if i >= n or j >= m:
+                return 0 if choose else -inf
+            return max(a[i] * b[j] + dp(i + 1, j + 1, True), dp(i + 1, j, choose), dp(i, j + 1, choose))
 
-        return dp()
+        return dp(0, 0, False)
