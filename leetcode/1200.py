0a86628c5a35110466561fdbee9f7fd751eeca9e
simonesestili <yolodoggy911@gmail.com>
2026-01-25 19:31:01 -0500

    1200

--- leetcode/1200.py
+++ leetcode/1200.py
@@ -1,11 +1,9 @@
 class Solution:
     def minimumAbsDifference(self, arr):
-        arr, best, ans = sorted(arr), float('inf'), []
+        ans, arr = [], sorted(arr)
         for i in range(1, len(arr)):
-            diff = arr[i] - arr[i - 1]
-            if diff < best:
-                ans = [(arr[i - 1], arr[i])]
-                best = diff
-            elif diff == best:
-                ans.append((arr[i - 1], arr[i]))
+            if not ans or arr[i] - arr[i-1] == ans[-1][1] - ans[-1][0]:
+                ans.append([arr[i-1], arr[i]])
+            elif ans and arr[i] - arr[i-1] < ans[-1][1] - ans[-1][0]:
+                ans = [[arr[i-1], arr[i]]]
         return ans
