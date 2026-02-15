4ea9b31476a077d35ddd2e1c86939bf7b3595b92
simonesestili <yolodoggy911@gmail.com>
2026-02-15 06:56:03 -0500

    67

--- leetcode/67.py
+++ leetcode/67.py
@@ -1,9 +1,9 @@
 class Solution:
-    def addBinary(self, b1, b2):
-        ans, carry = [], 0
-        for a, b in zip_longest(b1[::-1], b2[::-1], fillvalue='0'):
-            carry += (a == '1') + (b == '1')
-            ans.append(str(carry & 1))
-            carry >>= 1
-        if carry: ans.append(str(carry))
-        return ''.join(ans[::-1])
+    def addBinary(self, a, b):
+        ans, rem = [], 0
+        for x, y in zip_longest(reversed(a), reversed(b), fillvalue='0'):
+            rem += int(x == '1') + int(y == '1')
+            ans.append(str(rem & 1))
+            rem >>= 1
+        if rem: ans.append('1')
+        return ''.join(reversed(ans))
