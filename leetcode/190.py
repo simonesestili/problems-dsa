e65a47f8678aeb706e604b14ed247e016467e646
simonesestili <yolodoggy911@gmail.com>
2026-02-15 19:40:00 -0500

    190

--- leetcode/190.py
+++ leetcode/190.py
@@ -1,8 +1,8 @@
 class Solution:
     def reverseBits(self, n):
-        for i in range(16):
-            left, right = n >> (31 - i) & 1, n >> i & 1
-            n &= (1 << 32) - (1 << 31 - i) - (1 << i) - 1
-            n |= (left << i) + (right << 31 - i)
+        ans = 0
+        for _ in range(32):
+            ans = (ans << 1) | (n & 1)
+            n >>= 1
+        return ans
 
-        return n
