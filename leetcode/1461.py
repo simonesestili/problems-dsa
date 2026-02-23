424b2f2bba921d57f6a1bd084644e30591af0911
simonesestili <yolodoggy911@gmail.com>
2026-02-22 19:58:06 -0500

    1461

--- leetcode/1461.py
+++ leetcode/1461.py
@@ -1,15 +1,3 @@
 class Solution:
     def hasAllCodes(self, s, k):
-        if len(s) < k: return False
-        req, mask = 1 << k, (1 << k-1) - 1
-
-        seen, curr = set(), 0
-        for i, x in enumerate(s):
-            curr = (curr << 1) + (x == '1')
-            if i < k-1: continue
-            req -= curr not in seen
-            seen.add(curr)
-            if not req: return True
-            curr &= mask
-
-        return False
+        return len({s[i:i+k] for i in range(len(s) - k + 1)}) == 1 << k
