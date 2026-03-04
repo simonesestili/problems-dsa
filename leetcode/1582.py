ad219e8b550e302ac5a15438aad218997aedbf57
simonesestili <yolodoggy911@gmail.com>
2026-03-04 07:47:17 -0500

    1582

--- leetcode/1582.py
+++ leetcode/1582.py
@@ -1,16 +1,11 @@
-class Solution(object):     
+class Solution:
     def numSpecial(self, mat):
         m, n = len(mat), len(mat[0])
         rows, cols = [0] * m, [0] * n
-        for row in range(m):
-            for col in range(n):
-                if mat[row][col]:
-                    rows[row] += 1
-                    cols[col] += 1
 
-        count = 0
-        for row in range(m):
-            for col in range(n):
-                if rows[row] == 1 and cols[col] == 1 and mat[row][col]:
-                    count += 1
-        return count
+        for r in range(m):
+            for c in range(n):
+                rows[r] += mat[r][c]
+                cols[c] += mat[r][c]
+
+        return sum(mat[r][c] and rows[r] == cols[c] == 1 for c in range(n) for r in range(m))
