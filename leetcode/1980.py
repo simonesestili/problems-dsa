b49554741c377166e5ec39a11f9062522b95e89d
simonesestili <yolodoggy911@gmail.com>
2026-03-08 08:04:05 -0400

    1980

--- leetcode/1980.py
+++ leetcode/1980.py
@@ -1,3 +1,3 @@
 class Solution:
     def findDifferentBinaryString(self, nums):
-        return ''.join('1' if nums[i][i] == '0' else '0' for i in range(len(nums)))
+        return ''.join(str(int(nums[i][i]) ^ 1) for i in range(len(nums)))
