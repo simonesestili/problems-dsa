8184151bfd46d5a30cd951693be713dd51c81ec7
simonesestili <yolodoggy911@gmail.com>
2026-04-13 13:54:03 -0700

    1848

--- leetcode/1848.py
+++ leetcode/1848.py
@@ -1,8 +1,6 @@
 class Solution:
-    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
-        for i in range(len(nums)):
-            if start - i >= 0 and nums[start - i] == target:
-                return i
-            if start + i < len(nums) and nums[start + i] == target:
-                return i
-        return None
+    def getMinDistance(self, nums, target, start):
+        n = len(nums)
+        for d in range(n):
+            if 0 <= start - d and target == nums[start-d] or start + d < n and target == nums[start+d]:
+                return d
